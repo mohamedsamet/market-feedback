4f02de564e6b9524c5780ccf68989a26a96658d8
samsif <samet.development@gmail.com>
2026-03-10 14:39:17 +0100

    869cam9ed: Add consumer class

---
+++ src/main/java/com/yesmind/agent/ai/market_feedback/consumer/Consumer.java
@@ -0,0 +1,16 @@
+package com.yesmind.agent.ai.market_feedback.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer implements IConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(Consumer.class);
+
+    @Override
+    public void consume() {
+        // TODO Logic
+    }
+}

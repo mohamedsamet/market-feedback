8452d5f3c0e94879b360aa067c6bc1a3f3590be7
dammakhiba <hibadammak6@gmail.com>
2026-03-16 14:08:57 +0100

    le reste de changes

---
+++ src/main/java/com/yesmind/agent/ai/market_feedback/consumer/rest/news/Article.java
@@ -0,0 +1,10 @@
+package com.yesmind.agent.ai.market_feedback.consumer.rest.news;
+
+import lombok.Data;
+
+@Data
+public class Article {
+    private String title;
+    private String description;
+    private String url;
+}
--- src/main/java/com/yesmind/agent/ai/market_feedback/domain/model/MarketEvent.java
+++ src/main/java/com/yesmind/agent/ai/market_feedback/domain/model/MarketEvent.java
@@ -3,8 +3,11 @@ package com.yesmind.agent.ai.market_feedback.domain.model;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+
 
 import java.time.LocalDateTime;
+@Builder
 
 @Getter
 @NoArgsConstructor
